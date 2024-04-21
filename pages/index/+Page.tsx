ec63926c37d52ff001e5d97966763098a8d6132d
Dale <dale.cudmore@gmail.com>
2024-04-21 16:47:42 -0400

    fix casing in import

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -1,7 +1,7 @@
 export { Page }
 import { useState } from 'react'
 import './index.css'
-import Booklist from "../../components/BookList";
+import BookList from "../../components/BookList";
 import BookDetailsPopup from '../../components/BookDetailsPopup';
 function Page() {
   const [showAddBook, setShowAddBook] = useState(false)
@@ -12,7 +12,7 @@ function Page() {
         <button onClick={() => setShowAddBook(true)} aria-label="Add book" className='add-book'>+ Add Book</button>
       </div>
       {showAddBook && <BookDetailsPopup book={null} hideOverlay={() => setShowAddBook(false)}/>}
-      <Booklist />
+      <BookList />
     </>
   )
 }
